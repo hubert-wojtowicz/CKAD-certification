e88fd2b4203ec611000706c03de5009b41d7b510
Hubert Wójtowicz <hwojtowicz@eshopworld.com>
2021-07-23 10:09:44 +0200

    enviroments settings for productivity

---
+++ practise-environment/update-kubectl-clients.sh
@@ -0,0 +1,6 @@
+# update kubectl client
+sudo -i
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl $(which kubectl)
+k version --short]
